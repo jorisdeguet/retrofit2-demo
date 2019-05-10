f7e5f16944259f678bbb7515fae0a426513348bc
Joris Deguet <joris.deguet@gmail.com>
2019-05-09 22:38:50 -0400

    Débogage et logging Okhttp

--- app/src/main/java/org/deguet/retrofit2_demo/http/RetrofitUtil.java
+++ app/src/main/java/org/deguet/retrofit2_demo/http/RetrofitUtil.java
@@ -1,5 +1,7 @@
 package org.deguet.retrofit2_demo.http;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -10,11 +12,20 @@ public class RetrofitUtil {
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client())
                 .baseUrl("https://api.github.com/")
                 .build();
 
         Service service = retrofit.create(Service.class);
         return service;
+    }
+
+    private static OkHttpClient client() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        return client;
+
     }
 
 }
